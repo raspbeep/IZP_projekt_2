ceee05b3cab97a0386642362f6c22db0fc25977a
Pavel Kratochvil <pavel4000@gmail.com>
2020-11-21 09:58:30 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/IZP_projekt_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IZP_projekt_2.dir/main.c.o"
+  "IZP_projekt_2"
+  "IZP_projekt_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IZP_projekt_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
